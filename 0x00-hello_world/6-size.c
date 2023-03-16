8501314cc19d0e3a2e7637aa2b428c40a5b0e259
Haile1221 <Haile1221>
2023-03-16 10:53:39 -0700

    print various sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*Return: Always 
+*/
 int main(void)
 {
 	printf("Size of a char: %i byte(S)\n", sizeof(char));
