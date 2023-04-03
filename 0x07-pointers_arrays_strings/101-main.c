2e2b0867490fc5bd115b4aed030cec5cacbb322b
Haile1221 <hzerfu10@gmail.com>
2023-04-03 06:03:46 -0700

    updated and final

---
+++ 0x07-pointers_arrays_strings/101-main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <openssl/md5.h>
+
+/**
+ * main - print exact password
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	unsigned char digest[MD5_DIGEST_LENGTH];
+	char string[] = "password";
+
+	MD5((unsigned char *)&string, strlen(string), (unsigned char *)&digest);
+
+	int i;
+
+	for (i = 0; i < MD5_DIGEST_LENGTH; i++)
+		printf("%02x", digest[i]);
+
+	return (0);
+}
