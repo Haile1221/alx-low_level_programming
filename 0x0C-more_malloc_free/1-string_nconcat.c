f42ea08ad0fbb68df3b3f27d3b6c113ac279ef85
Haile1221 <hzerfu10@gmail.com>
2023-11-23 04:18:41 -0800

    concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,28 +1,56 @@
 #include <stdlib.h>
 #include "main.h"
+
 /**
- * malloc_checked - Allocates memory using malloc
- * @b: Number of bytes to allocate
+ * string_nconcat - Concatenates n bytes of a string to another string
+ * @s1: String to append to
+ * @s2: String to concatenate from
+ * @n: Number of bytes from s2 to concatenate to s1
  *
- * Return: A pointer to the allocated memory
- *         If malloc fails, terminates with status value of 98
+ * Return: Pointer to the resulting string
  */
-void *malloc_checked(unsigned int b)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	/* Declare a pointer to hold the allocated memory */
-	void *ptr;
+	char *s; /* Pointer to hold the resulting string */
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	/* Allocate memory using malloc */
-	ptr = malloc(b);
+	/* Calculate the length of s1 */
+	while (s1 && s1[len1])
+		len1++;
 
-	/* Check if malloc was successful */
-	if (ptr == NULL)
+	/* Calculate the length of s2 */
+	while (s2 && s2[len2])
+		len2++;
+
+	/* Determine the size of memory needed for the resulting string */
+	if (n < len2)
+		s = malloc(sizeof(char) * (len1 + n + 1));
+	else
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
+
+	/* Check if memory allocation was successful */
+	if (!s)
+		return (NULL);
+
+	/* Copy characters from s1 to the resulting string */
+	while (i < len1)
 	{
-		/* If malloc failed, terminate with status value of 98 */
-		exit(98);
+		s[i] = s1[i];
+		i++;
 	}
 
-	/* Return the pointer to the allocated memory */
-	return (ptr);
+	/* Copy the first n bytes of s2 to the resulting string */
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
+
+	/* If n is greater or equal to len2, use the entire string s2 */
+	while (n >= len2 && i < (len1 + len2))
+		s[i++] = s2[j++];
+
+	/* Null-terminate the resulting string */
+	s[i] = '\0';
+
+	/* Return the pointer to the resulting string */
+	return (s);
 }
 
