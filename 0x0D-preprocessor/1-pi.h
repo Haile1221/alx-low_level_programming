0f63da59950708ec3549b8dbbd68e8b2e5bf045a
Haile1221 <hzerfu10@gmail.com>
2023-11-27 05:51:56 -0800

    updated

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,12 @@
+#ifndef PI_H
+#define PI_H
+
+/* pi.h */
+/* If the header file has not been included before */
+
+/* Define the macro PI as an abbreviation for the token 3.14159265359 */
+#define PI 3.14159265359
+
+/* End the conditional inclusion directive */
+#endif /* PI_H */
+
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,8 @@
+#ifndef SUM_H
+#define SUM_H
+
+/* Define the SUM macro to compute the sum of two numbers */
+#define SUM(x, y) ((x) + (y))
+
+#endif /* SUM_H */
+
